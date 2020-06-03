74a98a28e380a5a3be341af25067b5353d548ece
Sergei Holovanenko <sergiobravos161@gmail.com>
2020-06-03 20:05:44 +0300

    s3 implementation

--- index.php
+++ index.php
@@ -2,6 +2,33 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-return function ($event) {
-    return 'Hello ' . ($event['name'] ?? 'world');
+use Aws\S3\S3Client;  
+use Aws\Exception\AwsException;
+use Aws\S3\Exception\S3Exception;
+
+return function ($keyname) {
+    $bucket = 'images2recognition';
+
+    $credentials = [
+        'key' => AWS_KEY,
+        'secret' => AWS_SECRET
+    ];
+
+    $s3Client = new S3Client([
+        'version' => 'latest',
+        'region'  => AWS_REGION,
+        'credentials' => $credentials
+    ]);
+
+    try {
+        $result = $s3Client->getObject([
+            'Bucket' => $bucket,
+            'Key'    => $keyname
+        ]);
+
+        return $result['ContentType'];
+
+    } catch (Exception $e) {
+        return $e->getMessage() . PHP_EOL;
+    }
 };
